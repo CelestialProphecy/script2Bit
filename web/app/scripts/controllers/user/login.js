8d2f068d034cdb6fe1e92422356b921b2e2be906
Rajat <rajat@ubuntu.(none)>
2016-02-21 13:44:53 +0530

    Script break down

--- web/app/scripts/controllers/user/login.js
+++ web/app/scripts/controllers/user/login.js
@@ -7,11 +7,16 @@
  * # UserLoginCtrl
  * Controller of the script2Bit
  */
-script2Bit.controller('UserLoginCtrl', ['$scope', function ($scope) {
-
+script2Bit.controller('UserLoginCtrl', ['$scope', '$http', function ($scope, $http) {
+  var file;
   $scope.init = function() {
+    $http.get('../../../files/scriptFiles/Big-Fish.txt').success(function(data) {
+      file = data;
+      console.log(file);
+    });
+    var output = fountain.parse(file);
+    console.log(output);
   };
-
   $scope.init();
 
   }]);
